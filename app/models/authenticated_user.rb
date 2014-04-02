09600da4093a13832f3893de6073271e7289637b
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-02 08:00:01 -0400

    make finding users available as module method
    Easier to access; I shouldn't need to know all the details to call this.

--- app/models/authenticated_user.rb
+++ app/models/authenticated_user.rb
@@ -1,4 +1,8 @@
 require 'authenticated_user/stable_auth'
+require 'authenticated_user/user_lookup'
 
 module AuthenticatedUser
+  def self.find_or_create(stable_auth)
+    UserLookup.run(stable_auth)
+  end
 end
