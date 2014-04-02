3293788de8986fef0247c1aab9fbadf5373c6c1e
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-02 10:17:52 -0400

    add a log out (session destroy) route/action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,11 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session.clear
+    redirect_to unauthenticated_path
+  end
+
   protected
 
   def auth_credentials
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ClashOfTheCoders::Application.routes.draw do
   get '/auth/stable/callback', to: 'sessions#create'
-  resource :session, only: [:new]
+  resource :session, only: [:new, :destroy]
+
   resource :unauthenticated, only: :show, controller: 'unauthenticated'
 
   root 'application#index'
