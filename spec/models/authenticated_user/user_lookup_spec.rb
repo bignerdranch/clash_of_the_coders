cc45be68044b1d5ec50e369197d31eaa93fd5a2a
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-02 21:32:17 -0400

    fix busted test based on 'refactoring'
    boo!

--- spec/models/authenticated_user/user_lookup_spec.rb
+++ spec/models/authenticated_user/user_lookup_spec.rb
@@ -20,14 +20,17 @@ describe AuthenticatedUser::UserLookup do
     expect(user_lookup.run).to eq(user)
   end
 
-  it 'returns a newly created user when there is not  a user with the email address' do
+  it 'returns a newly created user when there is not user with the email address' do
     allow(stable_auth).to receive(:email)
       .and_return(email)
+    allow(stable_auth).to receive(:name)
+      .and_return('any name')
+    allow(stable_auth).to receive(:uid)
+      .and_return(1)
     allow(user_factory).to receive(:where)
       .with(email: email)
       .and_return(Array.new)
     allow(user_factory).to receive(:create)
-      .with(stable_auth)
       .and_return(user)
 
     expect(user_lookup.run).to eq(user)
