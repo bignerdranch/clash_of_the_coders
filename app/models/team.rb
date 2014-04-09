d244f7d1d8b9bcab445d4ed5e65215339f29cba1
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-09 10:11:03 -0400

    add a scope to pull out this years teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,9 @@ class Team < ActiveRecord::Base
   validates :name, presence: true
 
   belongs_to :competition_year, inverse_of: :teams
+
+  scope :for_current_year, -> {
+    includes(:competition_year)
+      .where(competition_years: { year: Date.current.year })
+  }
 end
