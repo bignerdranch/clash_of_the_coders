58748506b572237e083bf10cddb9b5198ed0415e
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-11 22:33:51 -0400

    show proper error messages on team form

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ActiveRecord::Base
+  MEMBER_LIMIT = 4
   validates :competition_year_id, presence: true
   validates :name, presence: true
+  validate :member_limit
 
   belongs_to :competition_year, inverse_of: :teams
   has_many :members, through: :team_members, source: :user
@@ -10,4 +12,10 @@ class Team < ActiveRecord::Base
     includes(:competition_year)
       .where(competition_years: { year: Date.current.year })
   }
+
+  def member_limit
+    if members.count > MEMBER_LIMIT
+      errors.add(:members, "Teams can have no more than #{MEMBER_LIMIT} members.")
+    end
+  end
 end
