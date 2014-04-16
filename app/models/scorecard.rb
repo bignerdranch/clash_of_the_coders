4bcdf69628cf83654328cdd50d970d4e595d09ef
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-15 21:56:22 -0400

    add an object that can create batches of scores
    Scorecard.

---
+++ app/models/scorecard.rb
@@ -0,0 +1,22 @@
+class Scorecard
+  def self.create(scores: nil, user: nil)
+    new(user: user)
+      .create(scores: scores)
+  end
+
+  def initialize(user: nil)
+    @user = user
+  end
+
+  def create(scores)
+    scores.each do |score|
+      score_obj = OpenStruct.new(score)
+      Score.create(team_id: score_obj.id,
+        user: user,
+        value: score_obj.score)
+    end
+  end
+
+  private
+  attr_reader :user
+end
