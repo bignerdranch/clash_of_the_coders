80364fd5245598c4b9ec5265047937e6adfa2c17
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 07:26:25 -0400

    add a route/action/view for scorecard form
    At this point this does nothing...

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -1,4 +1,14 @@
 class ScorecardsController < ApplicationController
+  def create
+  end
+
   def new
+    @scorecard = Scoring::Form.new(teams: teams)
+  end
+
+  private
+
+  def teams
+    @teams ||= Team.for_current_year
   end
 end
