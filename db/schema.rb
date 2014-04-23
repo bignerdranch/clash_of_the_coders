7d6dc44916215fbb2bf6d956cf00d773e55ff650
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 07:23:19 -0400

    users can only score each team once, says the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416015653) do
+ActiveRecord::Schema.define(version: 20140423022249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140416015653) do
   end
 
   add_index "scores", ["team_id"], name: "index_scores_on_team_id", using: :btree
+  add_index "scores", ["user_id", "team_id"], name: "index_scores_on_user_id_and_team_id", using: :btree
   add_index "scores", ["user_id"], name: "index_scores_on_user_id", using: :btree
 
   create_table "team_members", force: true do |t|
