dc21f1106db4ca0e5fe9c8906bae3f15597224bf
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 10:02:34 -0400

    add by_user scope to Score

--- app/models/score.rb
+++ app/models/score.rb
@@ -7,4 +7,8 @@ class Score < ActiveRecord::Base
 
   belongs_to :team, inverse_of: :scores
   belongs_to :user, inverse_of: :scores
+
+  scope :by_user, ->(user) {
+    where(user: user)
+  }
 end
---
+++ spec/models/score_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_spec_helper'
+
+describe Score do
+  let(:user) { FactoryGirl.create(:user) }
+  let!(:score_1) { FactoryGirl.create(:score, user: user) }
+  let!(:score_2) { FactoryGirl.create(:score) }
+
+  specify '.by_user' do
+    expect(Score.by_user(user)).to match_array([score_1])
+  end
+end
