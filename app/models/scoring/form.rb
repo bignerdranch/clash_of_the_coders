a44d14a0d4956f466f38db0d6c38608fc66a8e5b
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 09:01:28 -0400

    handle the happy case (with mocks) for saving form

--- app/models/scoring/form.rb
+++ app/models/scoring/form.rb
@@ -8,9 +8,13 @@ module Scoring
       ActiveModel::Name.new(self, nil, 'Scorecard')
     end
 
-    attr_reader :teams
-    def initialize(teams: nil)
+    attr_reader :scores, :score_processor, :teams, :user, :validator
+    def initialize(scores: nil, teams: nil, user: nil, validator: Scoring::Validator, score_processor: Scoring::Processor)
+      @scores = scores
+      @score_processor = score_processor
       @teams = teams
+      @user = user
+      @validator = validator
     end
 
     def persisted?
@@ -18,6 +22,9 @@ module Scoring
     end
 
     def persist
+      if validator.valid?
+        score_processor.save
+      end
     end
 
     def submit
