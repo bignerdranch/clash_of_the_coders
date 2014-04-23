91f739793b9446b4c7658e6968a48f46afde9581
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 09:49:03 -0400

    add a dead-simple validator for form

---
+++ app/models/scoring/validator.rb
@@ -0,0 +1,26 @@
+module Scoring
+  class Validator
+    attr_reader :scores, :user
+    def initialize(scores: nil, score_factory: Score, team_factory: Team, user: nil)
+      @scores = scores
+      @score_factory = score_factory
+      @team_factory = team_factory
+      @user = user
+    end
+
+    def valid?
+      scores_by_user.count != teams_to_be_scored.count
+    end
+
+    private
+    attr_reader :score_factory, :team_factory
+
+    def scores_by_user
+      score_factory.by_user(user)
+    end
+
+    def teams_to_be_scored
+      team_factory.in_same_year(scores.first.id)
+    end
+  end
+end
---
+++ spec/models/scoring/validator_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'scoring/validator'
+
+describe Scoring::Validator do
+  let(:team_factory) { double('Team') }
+  let(:score_factory) { double('Score') }
+  let(:user) { double('User', id: 1) }
+  let(:team_1) { double('Team', id: 1) }
+  let(:team_2) { double('Team', id: 2) }
+  let(:team_1_score) { double('TeamScore', id: team_1.id, score: 10) }
+  let(:team_2_score) { double('TeamScore', id: team_2.id, score: 20) }
+  let(:scores) { [team_1_score, team_2_score] }
+
+  it 'returns true when the user has voted for every team once' do
+    allow(team_factory).to receive(:in_same_year)
+      .with(team_1.id)
+      .and_return([team_1, team_2])
+    allow(score_factory).to receive(:by_user)
+      .with(user)
+      .and_return(Array.new)
+
+    validator = Scoring::Validator.new(scores: scores,
+      team_factory: team_factory,
+      score_factory: score_factory,
+      user: user)
+
+    expect(validator.valid?).to be_truthy
+  end
+end
