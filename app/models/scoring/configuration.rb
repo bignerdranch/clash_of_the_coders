20b6b0db95013cace7c42b3ad534c9292a245372
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 21:58:48 -0400

    provide sensible defaults for configuration

--- app/models/scoring/configuration.rb
+++ app/models/scoring/configuration.rb
@@ -1,7 +1,23 @@
 module Scoring
   class Configuration < OpenStruct
-    def initialize(args)
+    def initialize(args={})
       super(args)
+    end
+
+    def score_factory
+      @table[:score_factory] || Score
+    end
+
+    def team_factory
+      @table[:team_factory] || Team
+    end
+
+    def validator
+      @table[:validator] || Scoring::Validator.new(scores: scores, user: user)
+    end
+
+    def score_processor
+      @table[:score_processor] || Scoring::Processor
     end
   end
 end
