356c3aab8c47581ae19f0f31ed19273517130645
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-04-23 20:56:52 -0400

    use config object in validator class

--- app/models/scoring/validator.rb
+++ app/models/scoring/validator.rb
@@ -1,11 +1,10 @@
+require 'scoring/configuration'
+
 module Scoring
   class Validator
-    attr_reader :scores, :user
-    def initialize(scores: nil, score_factory: Score, team_factory: Team, user: nil)
-      @scores = scores
-      @score_factory = score_factory
-      @team_factory = team_factory
-      @user = user
+    attr_reader :configuration
+    def initialize(configuration_hash)
+      @configuration = Scoring::Configuration.new(configuration_hash)
     end
 
     def valid?
@@ -15,12 +14,28 @@ module Scoring
     private
     attr_reader :score_factory, :team_factory
 
+    def scores
+      configuration.scores
+    end
+
     def scores_by_user
       @scores_by_user ||= score_factory.by_user(user)
     end
 
+    def score_factory
+      configuration.score_factory
+    end
+
+    def team_factory
+      configuration.team_factory
+    end
+
     def teams_to_be_scored
       @teams_to_be_scored ||= team_factory.in_same_year(scores.first.id)
+    end
+
+    def user
+      configuration.user
     end
   end
 end
